6d0c7a9ad7c41c4bcc638625b3de5a9982b84980
Uday-1402 <udayt1402@gmail.com>
2024-09-05 21:36:48 +0530

    Adding solution E , CF-971(Div-4)

--- codeforces.cpp
+++ codeforces.cpp
@@ -58,234 +58,43 @@ int min(int a, int b)
     return b;
 }
 
-// void solve(){
-//     int n;
-//     cin>>n;
+void solve(){
+    int n,k;
+    cin>>n>>k;
 
-//     int l = 0;
-//     int r = 1000000000;
+    int l = 1;
+    int r = n;
 
-//     vector<int> val(n);
-//     for(int i = 0;i<n;i++){
-//         cin>>val[i];
-//         // r = max(r,val[i]);
-//     }
+    int idx = 1;
 
-//     vector<int> p(n,-1);
+    function<pair<int,int>(int mid)> val = [&](int mid)->pair<int,int>{
+        int s = (n*(2*k + n - 1))/2;
+        int s1 = (mid*(2*k + mid - 1))/2;
+        int s2 = s - s1;
 
-//     for(int i = 1;i<n;i++){
-//         int x;
-//         cin>>x;
+        return {s1,s2};
+    };
 
-//         p[i] = --x;
-//     }
+    while(l<=r){
+        int mid = l + (r-l)/2;
+        
+        pair<int,int> p = val(mid);
+        int s1 = p.first;
+        int s2 = p.second;
+        if(s1 <= s2){
+            idx = mid;
+            l = mid+1;
+        }else{
+            r = mid -1 ;
+        }
+    }
 
-//     vector<int> adj[n];
+    auto p1 = val(idx);
+    auto p2 = val(idx+1);
 
-//     for(int i = 1;i<n;i++){
-//         if(p[i] != -1)
-//             adj[p[i]].push_back(i);
-//     }
+    cout<<min(abs(p1.first-p1.second),abs(p2.first-p2.second))<<endl;
 
-//     // function<int(int node)> dfs = [&](int node)->int{
-//     //     if(adj[node].size() == 0) return val[node];
-//     //     int mini = 1e9;
-//     //     for(auto adjNode:adj[node]){
-//     //         mini = min(mini,dfs(adjNode));
-//     //     }
-//     //     if(node == 0){
-//     //         return (val[node] + mini);
-//     //     }
-//     //     if(mini <= val[node]){
-//     //         return mini;
-//     //     }
-//     //     else return (mini + val[node])/2;
-//     // };
-
-//     // cout<<dfs(0)<<endl;
-
-//     function<bool(int node,int x)> check = [&](int node,int x)->bool{
-//         if(adj[node].size() == 0) return (val[node] >= x);
-//         bool fl = 1;
-//         if(node != 0){
-//             x = x + (max(0,x - val[node]));
-//         }
-//         if(x > 1e9) return false;
-//         for(auto adjNode: adj[node]){
-//             fl &= check(adjNode,x);
-//         }
-
-//         return fl;
-//     };
-
-//     int ans = 0;
-//     while(l<=r){
-//         int mid = l + (r-l)/2;
-
-//         if(check(0,mid)){
-//             ans = max(ans,mid);
-//             l = mid+1;
-//         }else{
-//             r = mid-1;
-//         }
-//     }
-
-//     cout<<val[0] + ans<<endl;
-// }
-
-// void solve()
-// {
-//     int n;
-//     cin >> n;
-
-//     vi adj[n];
-//     for (int i = 1; i < n; i++)
-//     {
-//         int x, y;
-//         cin >> x >> y;
-
-//         adj[x - 1].push_back(y - 1);
-//         adj[y - 1].push_back(x - 1);
-//     }
-
-//     string s;
-//     cin >> s;
-
-//     int cnt0 = 0,cnt1 = 0,cntq = 0;
-
-//     function<void(int node, int par)> dfs = [&](int node, int par) -> void
-//     {
-//         if(adj[node].size() == 1 && adj[node][0] == par){
-//             if(s[node] == '?') cntq++;
-//             else if(s[node] == '0') cnt0++;
-//             else cnt1++;
-//         }
-
-//         for(auto adjNode:adj[node]){
-//             if(adjNode != par)
-//                 dfs(adjNode,node);
-//         }
-//     };
-
-//     dfs(0,-1);
-
-//     int ans = 0;
-//     if(s[0] == '?'){
-//         cntq--;
-//         ans = max(cnt0,cnt1) + (cntq/2) + (cntq%2);
-//     }else if(s[0] == '0'){
-//         ans = cnt1 + (cntq/2) + (cntq%2);
-//     }else{
-//         ans = cnt0 + (cntq/2) + (cntq%2);
-//     }
-
-//     cout<<ans<<endl;
-// }
-
-// vector<int> isSq(1e6,0);
-
-// void solve(){
-//     int n;
-//     cin>>n;
-
-//     string s;
-//     cin>>s;
-
-//     if(isSq[n]){
-//         int len = sqrt(n);
-//         for(int i = 0;i<len;i++){
-//             for(int j = 0;j<len;j++){
-//                 if(i == 0 || i == len-1 || j == 0 || j == len - 1){
-//                     if(s[i*len + j] != '1'){
-//                         cout<<"No"<<endl;
-//                         return;
-//                     }
-//                 }else{
-//                     if(s[i*len + j] != '0'){
-//                         cout<<"No"<<endl;
-//                         return;
-//                     }
-//                 }
-//             }
-//         }
-//         cout<<"Yes"<<endl;
-//     }else{
-//         cout<<"No"<<endl;
-//     }
-// }
-
-// void solve(){
-//     int l,r;
-//     cin>>l>>r;
-
-//     if(l == r){
-//         cout<<1<<endl;
-//         return;
-//     }
-
-//     int k = 1;
-//     int cnt = 1;
-//     for(int i = l;i<=r;){
-//         if(i+k<=r)
-//             cnt++;
-//         i += k;
-//         k++;
-//     }
-
-//     cout<<cnt<<endl;
-// }
-
-// void solve(){
-//     int n;
-//     cin>>n;
-
-//     vi p(n+1);
-//     for(int i = 1;i<=n;i++){
-//         cin>>p[i];
-//     }
-
-//     string s;
-//     cin>>s;
-
-//     vi vis(n+1,0);
-//     map<int,int> isBlack;
-
-//     for(int i = 0;i<n;i++){
-//         if(s[i] == '0'){
-//             isBlack[p[i+1]] = 1;
-//         }
-//     }
-
-//     vvi a;
-//     for(auto pi:p){
-//         if(pi == 0) continue; 
-//         vi b;
-//         while(!vis[pi]){
-//             vis[pi] = 1;
-//             b.push_back(pi);
-//             pi = p[pi];
-//         }
-//         if(b.size()){
-//             a.push_back(b);
-//         }
-//     }
-
-//     vi ans(n+1,0);
-//     for(auto i:a){
-//         int nBlack = 0;
-//         for(auto j:i){
-//             if(isBlack[j]) nBlack++;
-//         }
-//         for(auto j:i){
-//             ans[j] = nBlack;
-//         }
-//     }
-
-//     for(int i = 1;i<=n;i++){
-//         cout<<ans[i]<<" ";
-//     }
-//     cout<<endl;
-// }
+}
 
 int32_t main()
 {
