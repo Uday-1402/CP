1df204e6b26f5ffdef294d7b661fc7dea0873a1d
Uday-1402 <udayt1402@gmail.com>
2024-09-13 01:50:58 +0530

    Adding Solutions B,C - CF-966.

--- codeforces.cpp
+++ codeforces.cpp
@@ -58,179 +58,105 @@ int min(int a, int b)
     return b;
 }
 
-// O(n^2) approach
-// void solve()
-// {
-//     int n,q;
-//     cin>>n>>q;
+int callBack(int p,int q){
+    return p&q;
+}
 
-//     vector<string> a(n);
+int higherOrder(int a,int b,function<int(int c,int d)> callBack){
+    return callBack(a,b);
+}
+
+//Problem B
+// void solve(){
+//     int n;
+//     cin>>n;
+
+//     vi a(n);
+//     in(a);
+
+//     cout<<a[n-1]<<" ";
+//     for(int i = 0;i<n-1;i++){
+//         cout<<a[i]<<" ";
+//     }
+
+//     cout<<endl;
+// }
+
+// Problem C
+// void solve(){
+//     int n,k;
+//     cin>>n>>k;
+    
+//     vpi a(n);
 //     for(int i = 0;i<n;i++){
-//         cin>>a[i];
+//         cin>>a[i].x;
 //     }
-
-//     map<char,int> mp;
-//     mp['B'] = 0;
-//     mp['G'] = 1;
-//     mp['R'] = 2;
-//     mp['Y'] = 3;
-
-//     vector<set<int>> b(4);
 //     for(int i = 0;i<n;i++){
-//         b[mp[a[i][0]]].insert(i);
-//         b[mp[a[i][1]]].insert(i);
-//     }
-//     vvi qry(q,vi(2));
-//     for(int i = 0;i<q;i++){
-//         int x,y;
-//         cin>>x>>y;
-
-//         qry[i][0] = --x;
-//         qry[i][1] = --y;
-
-//         // cout<<qry[i][0]<<" "<<qry[i][1]<<endl;
+//         cin>>a[i].y;
 //     }
 
-//     vector<vector<int>> dp(n,vi(n,1e9));
-//     for(int i = 0;i<n;i++){
-//         dp[i][i] = 0;
-//     }
-//     vi vis(n,0);
-
-//     function<int(int node,int dest)> dfs = [&](int node,int dest)->int{
-//         if(dp[node][dest] != 1e9) return dp[node][dest];
-//         int cx1 = a[node][0],cx2 = a[node][1], cy1 = a[dest][0], cy2 = a[dest][1];
-
-//         if(cx1 == cy1 || cx1 == cy2 || cx2 == cy1 || cx2 == cy2){
-//             return abs(node - dest);
+//     sort(all(a));
+    
+//     function<int(int i)> medianIdx = [&](int i)->int{
+//         int val = (n - 1)/2 + (n-1)%2;
+//         if(i < val){
+//             return val;
 //         }
-//         if(vis[node]) return 1e9;
+//         return val -1;
+//     };
 
-//         vis[node] = 1;
-//         int ans = 1e9;
-//         for(int i = 0;i<2;i++){
-//             for(auto adjNode:b[mp[a[node][i]]]){
-//                 if(adjNode != node && !vis[adjNode]){
-//                     ans = min(ans,abs(node - adjNode) + dfs(adjNode,dest));
+//     function<bool(int num)> check = [&](int num)->int{
+//         int reqCnt = (n + 1)/2 + 1;
+//         int remOps = k;
+//         for(int i = n-1;i>=0;i--){
+//             if(a[i].x >= num){
+//                 reqCnt--;
+//             }else if(a[i].y){
+//                 if(num - a[i].x <= remOps){
+//                     remOps -= (num - a[i].x);
+//                     reqCnt--;
 //                 }
 //             }
 //         }
-//         vis[node] = 0;
-
-//         dp[node][dest] = ans;
-//         dp[dest][node] = ans;
-//         return ans;
+//         return reqCnt <= 0;
 //     };
 
-//     for(int i = 0;i<q;i++){
-//         int x = qry[i][0], y = qry[i][1];
-
-//         int cx1 = a[x][0],cx2 = a[x][1], cy1 = a[y][0], cy2 = a[y][1];
-
-//         if(cx1 == cy1 || cx1 == cy2 || cx2 == cy1 || cx2 == cy2){
-//             dp[x][y] = abs(x - y);
-//             dp[y][x] = abs(x - y);
-//             cout<<abs(x-y)<<endl;
-//         }else{
-//             if(dp[x][y] != 1e9){
-//                 cout<<dp[x][y]<<endl;
-//             }else if(dp[y][x] != 1e9){
-//                 cout<<dp[y][x]<<endl;
-//             }else{
-//                 int ans = dfs(x,y);
-//                 if(ans >= 1e9) cout<<-1<<endl;
-//                 else cout<<ans<<endl;
-//             }
+//     int ans = a[n-1].x + a[medianIdx(n-1)].x;
+//     // cout<<ans<<"*1"<<endl;
+//     for(int i = n-1;i>=0;i--){
+//         if(a[i].y){
+//             ans = max(ans,a[i].x + k + a[medianIdx(i)].x);
+//             break;
 //         }
 //     }
+//     // cout<<ans<<"*2"<<endl;
+
+//     int l = 1;
+//     int r = 1e9;
+
+//     while(l<=r){
+//         int mid = l + (r-l)/2;
+
+//         if(check(mid)){
+//             ans = max(ans,a[n-1].x + mid);
+//             l = mid+1;
+//         }else{
+//             r = mid-1;
+//         }
+//     }
+
+//     // cout<<ans<<"*3"<<endl;
+//     cout<<ans<<endl;
 // }
-
-void solve()
-{
-    int n, q;
-    cin >> n >> q;
-
-    vector<string> a(n);
-
-    vector<vector<int>> mp(6);
-    vector<string> cases = {"BR", "BG", "BY", "GR", "GY", "RY"};
-
-    for(int i = 0;i<n;i++){
-        cin>>a[i];
-        if(a[i] == "BR") mp[0].push_back(i);
-        if(a[i] == "BG") mp[1].push_back(i);
-        if(a[i] == "BY") mp[2].push_back(i);
-        if(a[i] == "GR") mp[3].push_back(i);
-        if(a[i] == "GY") mp[4].push_back(i);
-        if(a[i] == "RY") mp[5].push_back(i);
-    }
-
-    while (q--)
-    {
-        int p, q;
-        cin >> p >> q;
-
-        p--;
-        q--;
-
-        if (p > q)
-            swap(p, q);
-
-        set<char> st;
-        for (auto i : a[p])
-            st.insert(i);
-        for (auto i : a[q])
-            st.insert(i);
-
-        if (st.size() < 4)
-        {
-            cout << q - p << endl;
-            continue;
-        }
-
-        int l = -1, r = n;
-        for (int e = 0;e<6;e++)
-        { 
-            string str = cases[e];
-            if (str == a[p] || str == a[q])
-                continue;
-            vector<int> st = mp[e];
-
-            auto it = upper_bound(all(st),p);
-            if(it != st.end()){
-                r = min(r,*it);
-            }
-            if(it != st.begin()){
-                it--;
-                l = max(l,*it);
-            }
-        }
-        if (l == -1 && r == n)
-        {
-            cout << -1 << endl;
-        }
-        else if (l == -1)
-        {
-            cout << abs(r - q) + abs(r - p) << endl;
-        }
-        else if (r == n)
-        {
-            cout << abs(l - p) + abs(l - q) << endl;
-        }
-        else
-        {
-            cout << min(abs(l - p) + abs(l - q), abs(r - p) + abs(r - q)) << endl;
-        }
-    }
-}
-
 
 int32_t main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    
+
+    // cout<<higherOrder(1,2,callBack);
+
     int t;
     cin >> t;
 
